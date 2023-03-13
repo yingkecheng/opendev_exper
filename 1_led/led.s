dbc26c21a27b788f94f6a657373caec7eb10472c
yingkevheng <yingkecheng0525@gmail.com>
2023-03-13 11:16:37 +0800

    add experiment_1 led

---
+++ 1_led/led.s
@@ -0,0 +1,48 @@
+.global _start
+
+_start:
+/* 1.使能全部时钟 */
+ldr r0, =0x020C4068
+ldr r1, =0xFFFFFFFF
+str r1, [r0]
+
+ldr r1, =0x020C406C
+str r1, [r0]
+
+ldr r1, =0x020C4070
+str r1, [r0]
+
+ldr r1, =0X020C4074
+str r1, [r0]
+
+ldr r1, =0X020C4078
+str r1, [r0]
+
+ldr r1, =0X020C407C
+str r1, [r0]
+
+ldr r1, =0X020C4080
+str r1, [r0]
+
+/* 2.设置GPIO1_IO03复用为GPIO1_IO3 */
+ldr r0, =0x020E0068
+ldr r1, =0x5
+str r1, [r0]
+
+/* 3.配置GPIO1_IO3的IO属性 */
+ldr r0, =0x020E02F4
+ldr r1, =0x10B0
+str r1, [r0]
+
+/* 4.设置GPIO1_IO3为输出 */
+ldr r0, =0x0209C004
+ldr r1, =0X0000008
+str r1, [r0]
+
+/* 5.打开LED0 设置GPIO1_IO3输出低电平 */
+ldr r0, =0x0209C000
+ldr r1, =0
+str r1, [r0]
+
+loop:
+    b loop
